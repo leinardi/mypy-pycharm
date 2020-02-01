8a1b6a784beb9ef6debd5f28c539f1ad1fd1c78b
Roberto Leinardi <leinardi@gmail.com>
2020-02-01 10:38:26 +0100

    Avoid subthread in inspection, as IDEA puts the whole inspection in a read lock

--- src/main/java/com/leinardi/pycharm/mypy/util/Async.java
+++ src/main/java/com/leinardi/pycharm/mypy/util/Async.java
@@ -18,27 +18,14 @@ package com.leinardi.pycharm.mypy.util;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.progress.ProgressManager;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 
-public class Async {
+public final class Async {
     private static final int FIFTY_MS = 50;
 
     private Async() {
-    }
-
-    @Nullable
-    public static <T> T asyncResultOf(@NotNull final Callable<T> callable,
-                                      @Nullable final T defaultValue) {
-        try {
-            return whenFinished(executeOnPooledThread(callable)).get();
-
-        } catch (Exception e) {
-            return defaultValue;
-        }
     }
 
     public static <T> Future<T> executeOnPooledThread(final Callable<T> callable) {
