dd32b9b8400c8b4f4353f8305de84d168603ab61
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2020-02-24 10:25:51 -0500

    Remove unused import

--- src/main/java/com/leinardi/pycharm/mypy/mpapi/MypyRunner.java
+++ src/main/java/com/leinardi/pycharm/mypy/mpapi/MypyRunner.java
@@ -46,7 +46,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.InterruptedIOException;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
