7f4c35a785c7d2c547a416d433d8e8417181e3de
Marti Raudsepp <marti@juffo.org>
2021-11-27 14:17:09 +0200

    Make Gradle check work
    'gradlew check' was failing for me for half a dozen different reasons.
    
    To be completely frank, I have no idea what I'm doing. I'm not familiar
    with the tools used in the Java world. I just kept applying solutions
    from StackOverflow until it seemed to work.

--- src/test/java/com/leinardi/pycharm/mypy/mpapi/MypyRunnerTest.java
+++ src/test/java/com/leinardi/pycharm/mypy/mpapi/MypyRunnerTest.java
@@ -5,16 +5,22 @@ import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class MypyRunnerTest {
+    private static InputStream stringToStream(String s) {
+        return new ByteArrayInputStream(s.getBytes(StandardCharsets.UTF_8));
+    }
+
     @Test
     public void testParseWithColon() throws IOException {
         String message = "Dict entry 0 has incompatible type \"int\": \"str\"; expected \"int\": \"int\"  [dict-item]";
         String input = "path/testfile.py:1:22: error: " + message + "\n";
         Issue parsed = new Issue("path/testfile.py", 1, 22, SeverityLevel.ERROR, message);
 
-        List<Issue> results = MypyRunner.parseMypyOutput(new ByteArrayInputStream(input.getBytes()));
+        List<Issue> results = MypyRunner.parseMypyOutput(stringToStream(input));
         Assert.assertArrayEquals(results.toArray(), new Issue[]{parsed});
     }
 }
